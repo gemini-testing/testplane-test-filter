d941c99171c94c7ba2a3d3281a0bdc4cf5d42703
Anton Usmansky <anton.usmansky@gmail.com>
2022-05-31 19:39:08 +0300

    Revert "Add filtering tests by their code"

--- test/utils.js
+++ test/utils.js
@@ -4,45 +4,43 @@ const fs = require('fs');
 const {readFile} = require('../lib/utils');
 
 describe('utils', () => {
-    describe('readFile', () => {
-        const sandbox = sinon.createSandbox();
+    const sandbox = sinon.createSandbox();
 
-        beforeEach(() => {
-            sandbox.stub(fs, 'readFileAsync');
-            sandbox.stub(console, 'warn');
+    beforeEach(() => {
+        sandbox.stub(fs, 'readFileAsync');
+        sandbox.stub(console, 'warn');
+    });
+
+    afterEach(() => sandbox.restore());
+
+    describe('if file with data does not exist', () => {
+        it('should not fail', async () => {
+            fs.readFileAsync.withArgs('foo.json').rejects();
+
+            await assert.isFulfilled(readFile('foo.json'));
         });
 
-        afterEach(() => sandbox.restore());
+        it('should return an empty array', async () => {
+            fs.readFileAsync.withArgs('foo.json').rejects();
 
-        describe('if file with data does not exist', () => {
-            it('should not fail', async () => {
-                fs.readFileAsync.withArgs('foo.json').rejects();
+            const data = await readFile('foo.json');
 
-                await assert.isFulfilled(readFile('foo.json'));
-            });
-
-            it('should return an empty array', async () => {
-                fs.readFileAsync.withArgs('foo.json').rejects();
-
-                const data = await readFile('foo.json');
-
-                assert.deepEqual(data, []);
-            });
+            assert.deepEqual(data, []);
         });
+    });
 
-        it('should fail if data from file can not be parsed', async () => {
-            fs.readFileAsync.withArgs('foo.json').resolves('foo');
+    it('should fail if data from file can not be parsed', async () => {
+        fs.readFileAsync.withArgs('foo.json').resolves('foo');
 
-            await assert.isRejected(readFile('foo.json'));
-        });
+        await assert.isRejected(readFile('foo.json'));
+    });
 
-        it('should parse data from file', async () => {
-            fs.readFileAsync.withArgs('foo.json').resolves('[]');
-            sandbox.stub(JSON, 'parse');
+    it('should parse data from file', async () => {
+        fs.readFileAsync.withArgs('foo.json').resolves('[]');
+        sandbox.stub(JSON, 'parse');
 
-            await readFile('foo.json');
+        await readFile('foo.json');
 
-            assert.calledWith(JSON.parse, '[]');
-        });
+        assert.calledWith(JSON.parse, '[]');
     });
 });
